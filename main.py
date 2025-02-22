12d1d4ef66f26f35525eade0a10beeadb98ab884
Alien7666 <jason6080717@gmail.com>
2025-02-22 09:06:36 +0800

    V0.1.0

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
             "runas", 
             sys.executable, 
             " ".join(sys.argv), 
-            None, 
+            None,
             1
         )
     else:
